1aa6f9e5d9b5f733620ffa7c69b500c8933090f1
Christopher Treviño <christophertrev@gmail.com>
2015-01-21 15:08:03 -0800

    (feat) Show current location on map

--- crumbsTest/ViewController.swift
+++ crumbsTest/ViewController.swift
@@ -7,17 +7,43 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation;
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        userLocation 
+        // Ask for Authorisation from the User.
+       
+
+        super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization();
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            
+        }
+        else{
+            println("Location service disabled");
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var locValue:CLLocationCoordinate2D = manager.location.coordinate
+        println("locations = \(locValue.latitude) \(locValue.longitude)")
     }
 
 
