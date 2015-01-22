fd0c11ecda3111b431fc5faf6cbd51a6af797a4f
Christopher Treviño <christophertrev@gmail.com>
2015-01-22 14:14:17 -0800

    Map Follows users location.

--- crumbsTest/ViewController.swift
+++ crumbsTest/ViewController.swift
@@ -41,7 +41,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         //create MKCoordinateRegion structure
         var coordinateRegion = MKCoordinateRegion(center: sfGiantsStadiumLocation, span: span)
         //set the region of the MKMapView object
-        map.setRegion(coordinateRegion, animated: true)
+//        map.setRegion(coordinateRegion, animated: true)
         
         //create an MKPointAnnotation object
         let sfGiantsStadiumAnnotation = MKPointAnnotation()
@@ -55,6 +55,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
         //add the annotation to the map
         map.addAnnotation(sfGiantsStadiumAnnotation)
+       
         
         
         
@@ -68,35 +69,42 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         var locValue:CLLocationCoordinate2D = manager.location.coordinate
         println("locations = \(locValue.latitude) \(locValue.longitude)")
+//        let sfGiantsStadiumLocation = CLLocationCoordinate2D(latitude: 37.783748, longitude: -122.409046)
+        //set a span to be used by the MKCoordinateRegion structure
+        let sfGiantsStadiumLocation = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
+        var span = MKCoordinateSpanMake(0.01, 0.01)
+        var coordinateRegion = MKCoordinateRegion(center: sfGiantsStadiumLocation, span: span)
+       
+        map.setRegion(coordinateRegion, animated: true)
     }
 
     class CustomPointAnnotation: MKPointAnnotation {
         var imageName: String!
     }
     
-    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
-        if (annotation is MKUserLocation){
-        //if annotation is not an MKPointAnnotation (eg. MKUserLocation),
-        //return nil so map draws default view for it (eg. blue dot)...
-        return nil
-        }
-    
-    
-    let reuseId = "test"
-    
-    var anView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
-    if anView == nil {
-        anView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-        anView.image = UIImage(named:"/Users/christophertrev/Desktop/xCode/crumbsTest/Map-Marker-Flag-2-Left-Pink-icon.png")
-        anView.centerOffset = CGPointMake(0, 00);
-    }
-    else {
-        //we are re-using a view, update its annotation reference...
-        anView.annotation = annotation
-    }
-    
-    return anView
-    }
+//    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+//        if (annotation is MKUserLocation){
+//        //if annotation is not an MKPointAnnotation (eg. MKUserLocation),
+//        //return nil so map draws default view for it (eg. blue dot)...
+//        return nil
+//        }
+//    
+//    
+//    let reuseId = "test"
+//    
+//    var anView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+//    if anView == nil {
+//        anView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+//        anView.image = UIImage(named:"/Users/christophertrev/Desktop/xCode/crumbsTest/Map-Marker-Flag-2-Left-Pink-icon.png")
+//        anView.centerOffset = CGPointMake(0, 00);
+//    }
+//    else {
+//        //we are re-using a view, update its annotation reference...
+//        anView.annotation = annotation
+//    }
+//    
+//    return anView
+//    }
 
 
     
